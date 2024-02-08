626926e4bbd1ab2f8239bb28375c2fa3b990f0fe
Nurislam <gggrrraaa1235@gmail.com>
2024-02-08 14:59:31 +0600

    5th commit almost finish basic ui view connecting binding in MainActivity class

--- app/src/main/java/com/example/nurquizapp/MainActivity.kt
+++ app/src/main/java/com/example/nurquizapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.nurquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.core.content.ContextCompat
+import com.example.nurquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val window: Window = this@MainActivity.window
+        window.statusBarColor = ContextCompat.getColor(this@MainActivity,R.color.gray)
     }
 }
